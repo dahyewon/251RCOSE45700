349a730ac979946c0e1f69c6d01c63d3752006e8
Dahye Won <doall1102@gmail.com>
2025-04-03 00:27:54 +0900

    mod: ShapeButton -> Toolbar로 수정 및 css 추가

---
+++ src/view/Toolbar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { CanvasViewModel } from "../viewModel/CanvasViewModel";
+import { DrawingState, SelectState } from "../viewModel/CanvasState";
+import "./Toolbar.css";
+
+const Toolbar: React.FC<{ viewModel: CanvasViewModel }> = ({
+  viewModel,
+}) => {
+  return (
+    <div className="toolbar">
+      <button
+        className='tool-button'
+        onClick={() => {
+          viewModel.setShapeType("rectangle");
+          viewModel.setState(new DrawingState(viewModel));
+        }}
+      >
+        ▭ 사각형
+      </button>
+
+      <button
+        className='tool-button'
+        onClick={() => {
+          viewModel.setShapeType("ellipse");
+          viewModel.setState(new DrawingState(viewModel));
+        }}
+      >
+        ◯ 원
+      </button>
+
+      <button
+        className='tool-button'
+        onClick={() => {
+          viewModel.setShapeType("select");
+          viewModel.setState(new SelectState(viewModel));
+        }}
+      >
+        선택
+      </button>
+    </div>
+  );
+};
+
+export default Toolbar;
