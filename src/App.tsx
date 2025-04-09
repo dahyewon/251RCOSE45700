167081800094bd2f85f73718e12ae112dd59a676
whsung0330 <whsung0330@gmail.com>
2025-04-09 22:38:55 +0900

    mod: set resizeHandle as observer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Canvas from "./view/Canvas";
 import { CanvasViewModel } from "./viewModel/CanvasViewModel";
 import { CanvasModel } from "./model/CanvasModel";
 import Toolbar from "./view/Toolbar";
+import ResizeHandle from "./view/ResizeHandle";
 
 const App: React.FC = () => {
   const model = new CanvasModel();
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       {/* <h1></h1> */}
       <Canvas viewModel={viewModel} />
       <Toolbar viewModel={viewModel} />
+      <ResizeHandle viewModel={viewModel} />
     </div>
   );
 };
