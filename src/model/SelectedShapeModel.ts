d8fae69ae77942726fa1eac5d5f3e86e3ab28c4e
whsung0330 <whsung0330@gmail.com>
2025-04-20 21:53:23 +0900

    ref: selectedShapeModel 분리

---
+++ src/model/SelectedShapeModel.ts
@@ -0,0 +1,33 @@
+import { Shape } from "../entity/Shape";
+
+export class SelectedShapeModel {
+  private selectedShapes: Shape[] = [];
+
+  clearSelectedShapes() {
+    this.selectedShapes = [];
+  }
+
+  addSelectedShapes(shape: Shape) {
+    this.selectedShapes.push(shape);
+  }
+
+  getSelectedShapes(): Shape[] {
+    return [...this.selectedShapes];
+  }
+
+  moveSelectedShapes(dx: number, dy: number): void {
+    return this.selectedShapes.forEach((shape) => {
+      shape.move(dx, dy);
+    });
+  }
+
+  getSelectedShapesHandles(): { x: number; y: number; pos: string }[][] {
+    return this.selectedShapes.map((shape) => shape.getResizeHandles());
+  }
+
+  resizeSelectedShapes(x: number, y: number, pos: string): void {
+    return this.selectedShapes.forEach((shape) => {
+      shape.resize(x, y, pos);
+    });
+  }
+}
