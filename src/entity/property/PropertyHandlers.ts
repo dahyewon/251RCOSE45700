bf55750d3b3bc1689761ade89c69f034656c3615
Dahye Won <doall1102@gmail.com>
2025-05-03 22:45:33 +0900

    feat: AbstractShape 추상화 클래스, 속성 핸들러 분리

---
+++ src/entity/property/PropertyHandlers.ts
@@ -0,0 +1,96 @@
+import { PROPERTY_NAMES, PROPERTY_TYPES } from "../../constants";
+
+export interface Property {
+  type: string;
+  name: string;
+  value: string | number;
+}
+
+export interface PropertyHandler<T> {
+  type: string;
+  name: string;
+  getValue(shape: T): string | number;
+  setValue(shape: T, value: any): void;
+}
+
+export const CommonPropertyHandlers = {
+  HorizontalPos: <T extends { centerX: number; setCenterX(newX: number): void }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.HORIZONTAL_POS,
+    getValue: (shape) => Math.round(shape.centerX),
+    setValue: (shape, value) => shape.setCenterX(Number(value)),
+  }),
+  VerticalPos: <T extends { centerY: number; setCenterY(newY: number): void }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.VERTICAL_POS,
+    getValue: (shape) => Math.round(shape.centerY),
+    setValue: (shape, value) => shape.setCenterY(Number(value)),
+  }),
+  Width: <T extends { width: number; setWidth(newWidth: number): void }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.WIDTH,
+    getValue: (shape) => Math.round(shape.width),
+    setValue: (shape, value) => shape.setWidth(Number(value)),
+  }),
+  Height: <T extends { height: number; setHeight(newHeight: number): void }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.HEIGHT,
+    getValue: (shape) => Math.round(shape.height),
+    setValue: (shape, value) => shape.setHeight(Number(value)),
+  }),
+  Color: <T extends { color: string }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.COLOR,
+    name: PROPERTY_NAMES.COLOR,
+    getValue: (shape) => shape.color,
+    setValue: (shape, value) => { shape.color = value.toString(); },
+  }),
+  ShadowAngle: <T extends { shadowAngle: number; shadowRadius: number; shadowOffsetX: number; shadowOffsetY: number }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.SHADOW_ANGLE,
+    getValue: (shape) => Math.round(shape.shadowAngle * (180 / Math.PI)),
+    setValue: (shape, value) => {
+      const newAngle = Number(value) * (Math.PI / 180);
+      const radius = shape.shadowRadius;
+      shape.shadowOffsetX = radius * Math.cos(newAngle);
+      shape.shadowOffsetY = radius * Math.sin(newAngle);
+    },
+  }),
+  ShadowRadius: <T extends { shadowRadius: number; shadowAngle: number; shadowOffsetX: number; shadowOffsetY: number }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.SHADOW_RADIUS,
+    getValue: (shape) => shape.shadowRadius,
+    setValue: (shape, value) => {
+      const newRadius = Number(value);
+      const shadowAngle = shape.shadowAngle;
+      shape.shadowOffsetX = newRadius * Math.cos(shadowAngle);
+      shape.shadowOffsetY = newRadius * Math.sin(shadowAngle);
+    },
+  }),
+  ShadowBlur: <T extends { shadowBlur: number }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.SHADOW_BLUR,
+    getValue: (shape) => shape.shadowBlur,
+    setValue: (shape, value) => { shape.shadowBlur = Number(value); },
+  }),
+  ShadowColor: <T extends { shadowColor: string }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.COLOR,
+    name: PROPERTY_NAMES.SHADOW_COLOR,
+    getValue: (shape) => shape.shadowColor,
+    setValue: (shape, value) => { shape.shadowColor = value.toString(); },
+  }),
+};
+
+export const BorderedShapePropertyHandlers = {
+  BorderWidth: <T extends { borderWidth: number }>() => ({
+    type: PROPERTY_TYPES.NUMBER,
+    name: PROPERTY_NAMES.BORDER_WIDTH,
+    getValue: (shape: T) => Math.round(shape.borderWidth),
+    setValue: (shape: T, value: any) => { shape.borderWidth = Number(value); },
+  }),
+  BorderColor: <T extends { borderColor: string }>(): PropertyHandler<T> => ({
+    type: PROPERTY_TYPES.COLOR,
+    name: PROPERTY_NAMES.BORDER_COLOR,
+    getValue: (shape) => shape.borderColor,
+    setValue: (shape, value) => { shape.borderColor = value.toString(); },
+  }),
+};
--- src/entity/shape/Shape.ts
+++ src/entity/shape/Shape.ts
@@ -1,20 +1,148 @@
+import { Property, PropertyHandler } from "../property/PropertyHandlers";
+
 export interface Shape {
   id: number;
   startX: number;
   startY: number;
   endX: number;
   endY: number;
+  color: string;
+  readonly width: number;
+  readonly height: number;
+  readonly centerX: number;
+  readonly centerY: number;
+
   draw(ctx: CanvasRenderingContext2D | null): void;
   move(dx: number, dy: number): void;
+  setShadow(ctx: CanvasRenderingContext2D): void;
   getResizeHandles(): { x: number; y: number; pos: string }[];
   resize(dx: number, dy: number, pos: string): void;
   isPointInside(x: number, y: number): boolean;
   getProperties(): Property[];
-  setProperties(name: string, value: number): void;
+  setProperties(name: string, value: any): void;
 }
 
-export interface Property {
-  name: string;
-  value: string | number;
-  editable: boolean;
+export abstract class AbstractShape implements Shape {
+  constructor(
+      public id: number,
+      public startX: number,
+      public startY: number,
+      public endX: number,
+      public endY: number,
+      public color: string = "#000000",
+  ) {}
+  shadowColor: string = "#000000";
+  shadowOffsetX: number = 0;
+  shadowOffsetY: number = 0;
+  shadowBlur: number = 0;
+
+  get width(): number {
+      return Math.abs(this.endX - this.startX);
+  }
+  get height(): number {
+      return Math.abs(this.endY - this.startY);
+  }
+  get centerX(): number {
+      return (this.startX + this.endX) / 2
+  }
+  get centerY(): number {
+      return (this.startY + this.endY) / 2
+  }
+  get shadowAngle(): number {
+    return Math.atan2(this.shadowOffsetY, this.shadowOffsetX);
+  }
+  get shadowRadius(): number {
+    return Math.round(
+      Math.sqrt(
+        this.shadowOffsetX * this.shadowOffsetX +
+          this.shadowOffsetY * this.shadowOffsetY
+      )
+    );
+  }
+
+  move(dx: number, dy: number): void {
+      this.startX += dx;
+      this.startY += dy;
+      this.endX += dx;
+      this.endY += dy;
+  }
+  
+  setShadow(ctx: CanvasRenderingContext2D): void {
+    ctx.shadowColor = this.shadowColor;
+    ctx.shadowOffsetX = this.shadowOffsetX;
+    ctx.shadowOffsetY = this.shadowOffsetY;
+    ctx.shadowBlur = this.shadowBlur;
+  }
+
+  // 4개 꼭지점 기준
+  // Line은 override 필요
+  getResizeHandles(): { x: number; y: number; pos: string; }[] {
+      return [
+          { x: this.startX - 5, y: this.startY - 5, pos: "top-left" },
+          { x: this.endX - 5, y: this.startY - 5, pos: "top-right" },
+          { x: this.endX - 5, y: this.endY - 5, pos: "bottom-right" },
+          { x: this.startX - 5, y: this.endY - 5, pos: "bottom-left" },
+      ];
+  }
+  // Line은 override 필요
+  resize(dx: number, dy: number, pos: string): void {
+      switch (pos) {
+          case "top-left":
+            this.startX += dx;
+            this.startY += dy;
+            break;
+          case "top-right":
+            this.endX += dx;
+            this.startY += dy;
+            break;
+          case "bottom-right":
+            this.endX += dx;
+            this.endY += dy;
+            break;
+          case "bottom-left":
+            this.startX += dx;
+            this.endY += dy;
+            break;
+        }
+  }
+
+  abstract draw(ctx: CanvasRenderingContext2D): void;
+  abstract isPointInside(x: number, y: number): boolean;
+  // 혹시나 getProperties와 헷갈려서 밖에서 사용할까봐 protected로 구현
+  protected abstract getPropertyHandlers(): PropertyHandler<this>[];
+
+  getProperties(): Property[] {
+      return this.getPropertyHandlers().map(handler => ({
+          type: handler.type,
+          name: handler.name,
+          value: handler.getValue(this),
+      }));
+  }
+
+  setProperties(name: string, value: any): void {
+      const handler = this.getPropertyHandlers().find(h => h.name === name);
+      if (!handler) throw new Error(`Invalid property name: ${name}`);
+      handler.setValue(this, value);
+  }
+
+  setCenterX(newX: number): void {
+      const width = this.width;
+      this.startX = newX - width / 2;
+      this.endX = newX + width / 2;
+  }
+  setCenterY(newY: number): void {
+      const height = this.height;
+      this.startY = newY - height / 2;
+      this.endY = newY + height / 2;
+  }
+  setWidth(newWidth: number): void {
+      const centerX = this.centerX;
+      this.startX = centerX - newWidth / 2;
+      this.endX = centerX + newWidth / 2;
+  }
+  setHeight(newHeight: number): void {
+      const centerY = this.centerY;
+      this.startY = centerY - newHeight / 2;
+      this.endY = centerY + newHeight / 2;
+  }
 }
