ce912d3fe0b5a49d4b98409565ddee7c10b52f96
whsung0330 <whsung0330@gmail.com>
2025-05-22 17:14:18 +0900

    mod: addTemplateShapeCommand, CanvasResetCommand가 viewmodel을 받도록 수정

--- src/command/AddTemplateShapeCommand.ts
+++ src/command/AddTemplateShapeCommand.ts
@@ -1,16 +1,20 @@
 import { SelectedShapeModel } from "../model/SelectedShapeModel";
 import { ShapeModel } from "../model/ShapeModel";
 import { CanvasViewModel } from "../viewModel/CanvasViewModel";
+import { ToolbarViewModel } from "../viewModel/ToolbarViewModel";
 import { Command } from "./Command";
 
 export class AddTemplateShapeCommand implements Command {
+  private shapeModel: ShapeModel;
+  private selectedShapeModel: SelectedShapeModel;
   constructor(
-    private canvasViewModel: CanvasViewModel,
-    private shapeModel: ShapeModel,
-    private selectedShapeModel: SelectedShapeModel,
+    private viewModel: CanvasViewModel | ToolbarViewModel,
     private type: string,
     private properties: any
-  ) {}
+  ) {
+    this.shapeModel = viewModel.shapeModel;
+    this.selectedShapeModel = viewModel.selectedShapeModel;
+  }
 
   execute(): void {
     const shape = this.shapeModel.addTemplateShape(this.type, this.properties);
--- src/command/CanvasResetCommand.ts
+++ src/command/CanvasResetCommand.ts
@@ -1,39 +1,32 @@
+import { CanvasModel } from "../model/CanvasModel";
 import { SelectedShapeModel } from "../model/SelectedShapeModel";
 import { ShapeModel } from "../model/ShapeModel";
 import { DrawState } from "../viewModel/canvasState/DrawState";
 import { CanvasViewModel } from "../viewModel/CanvasViewModel";
+import { ToolbarViewModel } from "../viewModel/ToolbarViewModel";
 import { Command } from "./Command";
 
 export class CanvasResetCommand implements Command {
-  private canvasViewModel: CanvasViewModel;
+  private toolbarViewModel: ToolbarViewModel;
+  private canvasModel: CanvasModel;
   private shapeModel: ShapeModel;
   private selectedShapeModel: SelectedShapeModel;
 
-  constructor(
-    canvasViewModel: CanvasViewModel,
-    shapeModel: ShapeModel,
-    selectedShapeModel: SelectedShapeModel
-  ) {
-    this.canvasViewModel = canvasViewModel;
-    this.shapeModel = shapeModel;
-    this.selectedShapeModel = selectedShapeModel;
+  constructor(toolbarViewModel: ToolbarViewModel) {
+    this.toolbarViewModel = toolbarViewModel;
+    this.canvasModel = toolbarViewModel.canvasModel;
+    this.shapeModel = toolbarViewModel.shapeModel;
+    this.selectedShapeModel = toolbarViewModel.selectedShapeModel;
   }
 
   execute(): void {
     this.shapeModel.clearShapes();
     this.selectedShapeModel.clearSelectedShapes();
-    this.canvasViewModel.setShapeType("rectangle"); // default 값으로
-    this.canvasViewModel.setState(
-      new DrawState(
-        this.canvasViewModel,
-        this.shapeModel,
-        this.selectedShapeModel,
-        this.canvasViewModel.getShapeType()
-      )
-    ); // default: 그리기 모드
+    this.canvasModel.setShapeType("rectangle"); // default 값으로
+    this.canvasModel.setState("DrawState"); // default: 그리기 모드
 
     // 만약 입력창이 남아있다면 삭제
-    this.canvasViewModel.notifyResetInput();
+    // this.canvasViewModel.notifyResetInput();
   }
 
   redo(): void {
