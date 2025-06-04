1dedad6eaf6899730700542b347073e8cdbab2f6
whsung0330 <whsung0330@gmail.com>
2025-06-04 16:55:56 +0900

    add: undo, redo execute method

--- src/command/CommandManager.ts
+++ src/command/CommandManager.ts
@@ -20,4 +20,20 @@ export class CommandManager {
       this.undoStack.push(command);
     }
   }
+
+  public executeUndo(): void {
+    const command = this.undoStack.pop();
+    if (command) {
+      command.undo();
+      this.redoStack.push(command);
+    }
+  }
+
+  public executeRedo(): void {
+    const command = this.redoStack.pop();
+    if (command) {
+      command.execute();
+      this.undoStack.push(command);
+    }
+  }
 }
