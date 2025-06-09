89c12f6792ecb85b2863312d28ea21c504c2c5ab
whsung0330 <whsung0330@gmail.com>
2025-06-09 17:50:53 +0900

    add: Group 클래스

---
+++ src/entity/shape/Group.ts
@@ -0,0 +1,119 @@
+import { ResizeHandlePosition } from "../../constants";
+import {
+  CommonPropertyHandlers,
+  Property,
+  PropertyHandler,
+} from "../property/PropertyHandlers";
+import { Shape } from "./Shape";
+
+export class Group implements Shape {
+  private children: Shape[] = [];
+  constructor(
+    public id: number,
+    public startX: number,
+    public startY: number,
+    public endX: number,
+    public endY: number
+  ) {}
+
+  get centerX(): number {
+    return (this.startX + this.endX) / 2;
+  }
+  get centerY(): number {
+    return (this.startY + this.endY) / 2;
+  }
+
+  add(target: Shape) {
+    this.children.push(target);
+
+    if (target.startX < this.startX) this.startX = target.startX;
+    if (target.startY < this.startY) this.startY = target.startY;
+    if (target.endX > this.endX) this.endX = target.endX;
+    if (target.endY > this.endY) this.endY = target.endY;
+  }
+
+  remove(target: Shape) {
+    this.children = this.children.filter((child) => child !== target);
+  }
+
+  draw(ctx: CanvasRenderingContext2D | null): void {
+    this.children.forEach((child) => {
+      child.draw(ctx);
+    });
+  }
+
+  move(dx: number, dy: number): void {
+    this.children.forEach((child) => {
+      child.move(dx, dy);
+    });
+  }
+
+  getResizeHandles(): { x: number; y: number; pos: ResizeHandlePosition }[] {
+    return [
+      {
+        x: this.startX - 5,
+        y: this.startY - 5,
+        pos: ResizeHandlePosition.TopLeft,
+      },
+      {
+        x: this.endX - 5,
+        y: this.startY - 5,
+        pos: ResizeHandlePosition.TopRight,
+      },
+      {
+        x: this.endX - 5,
+        y: this.endY - 5,
+        pos: ResizeHandlePosition.BottomRight,
+      },
+      {
+        x: this.startX - 5,
+        y: this.endY - 5,
+        pos: ResizeHandlePosition.BottomLeft,
+      },
+    ];
+  }
+
+  resize(dx: number, dy: number, pos: string): void {
+    this.children.forEach((child) => child.resize(dx, dy, pos));
+  }
+  isPointInside(x: number, y: number): boolean {
+    return (
+      x >= Math.min(this.startX, this.endX) &&
+      x <= Math.max(this.startX, this.endX) &&
+      y >= Math.min(this.startY, this.endY) &&
+      y <= Math.max(this.startY, this.endY)
+    );
+  }
+
+  setCenterX(newX: number): void {
+    const width = this.endX - this.startX;
+    this.startX = newX - width / 2;
+    this.endX = newX + width / 2;
+  }
+  setCenterY(newY: number): void {
+    const height = this.endY - this.startY;
+    this.startY = newY - height / 2;
+    this.endY = newY + height / 2;
+  }
+
+  getProperties(): Property[] {
+    return this.getPropertyHandlers().map((handler) => ({
+      type: handler.type,
+      name: handler.name,
+      value: handler.getValue(this),
+    }));
+  }
+
+  setProperties(name: string, value: any): void {
+    const handler = this.getPropertyHandlers().find((h) => h.name === name);
+    if (!handler) throw new Error(`Invalid property name: ${name}`);
+    handler.setValue(this, value);
+  }
+
+  protected getPropertyHandlers(): PropertyHandler<this>[] {
+    return [
+      CommonPropertyHandlers.HorizontalPos(),
+      CommonPropertyHandlers.VerticalPos(),
+    ];
+  }
+}
